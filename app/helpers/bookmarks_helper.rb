ab272fa11cc1bfaa296f4a6a6988e52627e2265e
Dafydd Francis <daveordref@gmail.com>
2011-02-09 20:32:16 +0000

    Changed the layout

--- app/helpers/bookmarks_helper.rb
+++ app/helpers/bookmarks_helper.rb
@@ -1,2 +1,11 @@
 module BookmarksHelper
+  def get_site(url)
+    #At first I was going to do the following with regex
+    #but then realised this was a much better way of doing it
+    require 'uri'
+    site_url = URI.parse(url)
+    site = Site.find_by_url("http://#{site_url.host}/")
+    site = Site.create(:url => "http://#{site_url.host}/", :title => get_title("http://#{site_url.host}/")) if site.nil?
+    site
+  end
 end
--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,5 +1,6 @@
 class Bookmark < ActiveRecord::Base
   validates_presence_of :url
+  validates_uniqueness_of :url
 
   has_one :site
 end
